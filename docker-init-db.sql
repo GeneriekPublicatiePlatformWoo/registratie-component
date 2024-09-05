f8f13d1c68eeb0609d019fcebb211fd0a956160b
Sergei Maertens <sergei@maykinmedia.nl>
2024-09-05 11:15:18 +0200

    :whale: Fix compose setup
    DB permissions were not set up properly for modern Postgres
    versions.

--- docker-init-db.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER woo_publications;
-CREATE DATABASE woo_publications;
-GRANT ALL PRIVILEGES ON DATABASE woo_publications TO woo_publications;
-GRANT USAGE, CREATE ON SCHEMA public TO woo_publications;
