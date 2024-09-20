cec70073cba05d4692d98499d32a2ff85b75dffa
bart-maykin <bart.burgman@maykinmedia.nl>
2024-09-20 15:02:58 +0200

    :card_file_box: [#11] added theme model

--- src/woo_publications/metadata/models.py
+++ src/woo_publications/metadata/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from ordered_model.models import OrderedModel
+from treebeard.mp_tree import MP_Node
 
 from .constants import InformationCategoryOrigins
 
@@ -60,6 +61,52 @@ class InformationCategory(OrderedModel):
     class Meta(OrderedModel.Meta):
         verbose_name = _("information category")
         verbose_name_plural = _("information categories")
+
+    def __str__(self):
+        return self.naam
+
+
+class Theme(MP_Node):
+    identifier = models.URLField(
+        _("identifier"),
+        help_text=_(
+            "The unique URI that identifies this theme in the overheid.nl value list. "
+            "For entries that have been added manually, an identifier is generated."
+        ),
+        max_length=255,
+        unique=True,
+        editable=False,
+        default=get_default_identifier,
+    )
+    naam = models.CharField(
+        _("name"),
+        help_text=_("The name of the theme."),
+        max_length=80,
+    )
+    publicatie_status_actief = models.BooleanField(
+        _("publication status active"),
+        help_text=_("A flag to display if this theme is active or not."),
+        default=False,
+        blank=False,
+    )
+    oorsprong = models.CharField(
+        _("origin"),
+        help_text=_(
+            "Determines where the theme is defined and sourced from, and how "
+            "the identifier should be interpreted. If the value list is the origin, the "
+            "theme can not be modified or deleted."
+        ),
+        choices=InformationCategoryOrigins.choices,
+        blank=False,
+        max_length=15,
+        default=InformationCategoryOrigins.custom_entry,
+    )
+
+    node_order_by = ("naam",)
+
+    class Meta:  # type: ignore
+        verbose_name = _("theme")
+        verbose_name_plural = _("theme's")
 
     def __str__(self):
         return self.naam
--- src/woo_publications/metadata/tests/factories.py
+++ src/woo_publications/metadata/tests/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from ..models import InformationCategory
+from ..models import InformationCategory, Theme
 
 
 class InformationCategoryFactory(factory.django.DjangoModelFactory):
@@ -8,3 +8,18 @@ class InformationCategoryFactory(factory.django.DjangoModelFactory):
 
     class Meta:  # type: ignore
         model = InformationCategory
+
+
+class ThemeFactory(factory.django.DjangoModelFactory):
+    naam = factory.Faker("word")
+
+    class Meta:  # type: ignore
+        model = Theme
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        # defer creation to treebeard instead of fuzzing the underlying DB fields
+        parent = kwargs.pop("parent", None)
+        if parent is not None:
+            return parent.add_child(**kwargs)  # noqa
+        return model_class.add_root(**kwargs)  # noqa
