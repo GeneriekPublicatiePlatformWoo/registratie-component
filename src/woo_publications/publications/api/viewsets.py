313b0ba5fb01bf69532fe6e09ccbde18d13beda9
bart-maykin <bart.burgman@maykinmedia.nl>
2024-09-26 16:31:19 +0200

    :sparkles: [#35] added publications crud endpoint with order query parameter

---
+++ src/woo_publications/publications/api/viewsets.py
@@ -0,0 +1,43 @@
+from django.utils.translation import gettext_lazy as _
+
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import permissions, viewsets
+
+from .filters import PublicationFilterSet
+from ..models import Publication
+from .serializer import PublicationSerializer
+
+
+@extend_schema(tags=["Publicaties"])
+@extend_schema_view(
+    list=extend_schema(
+        summary=_("All available publications."),
+        description=_("Returns a paginated result list of existing publications."),
+    ),
+    retrieve=extend_schema(
+        summary=_("Retrieve a specific publication."),
+        description=_("Retrieve a specific publication."),
+    ),
+    create=extend_schema(
+        summary=_("Create a publication."),
+        description=_("Create a publication."),
+    ),
+    partial_update=extend_schema(
+        summary=_("Update a publication partially."),
+        description=_("Update a publication partially."),
+    ),
+    update=extend_schema(
+        summary=_("Update a publication entirely."),
+        description=_("Update a publication entirely."),
+    ),
+    destroy=extend_schema(
+        summary=_("Destroy a publication."),
+        description=_("Destroy a publication."),
+    ),
+)
+class PublicationViewSet(viewsets.ModelViewSet):
+    queryset = Publication.objects.all()
+    serializer_class = PublicationSerializer
+    filterset_class = PublicationFilterSet
+    lookup_field = "uuid"
+    permission_classes = (permissions.AllowAny,)
