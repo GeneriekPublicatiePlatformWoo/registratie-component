301e8ffda5b3e4dec541ec4cb21ddb89e2236577
bart-maykin <bart.burgman@maykinmedia.nl>
2024-09-27 11:47:29 +0200

    :card_file_box: [#37] created document model
    Added the fields creatiedatum, bestandsformaat, bestandsnaam and
    bestandsomvang because they're already present in the mocks of the ODPC
    and we want to avoid mismatches.
    
    We will need these fields in due time anyway.

--- src/woo_publications/publications/models.py
+++ src/woo_publications/publications/models.py
@@ -38,3 +38,76 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.officiele_titel
+
+
+class Document(models.Model):
+    publicatie = models.ForeignKey(
+        Publication,
+        verbose_name=_("publication"),
+        help_text=_(
+            "The publication that this document belongs to. A publication may have "
+            "zero or more documents."
+        ),
+        on_delete=models.CASCADE,
+    )
+    identifier = models.CharField(
+        _("identifier"),
+        help_text=_("The (primary) unique identifier."),
+        max_length=255,
+        unique=True,
+    )
+    officiele_titel = models.CharField(
+        _("official title"),
+        max_length=255,
+    )
+    verkorte_titel = models.CharField(
+        _("short title"),
+        max_length=255,
+        blank=True,
+    )
+    omschrijving = models.TextField(_("description"), blank=True)
+    creatiedatum = models.DateField(
+        _("creation date"),
+        help_text=_(
+            "Date when the (physical) document came into existence. Not to be confused "
+            "with the registration timestamp of the document - the creation date is "
+            "typically *before* the registration date."
+        ),
+    )
+    bestandsformaat = models.CharField(
+        _("file format"),
+        max_length=255,
+        help_text=_(
+            "TODO - placeholder accepting anything, in the future this will be "
+            "validated against a reference value list."
+        ),
+        default="unknown",  # TODO: remove this once the formats are set up properly
+    )
+    bestandsnaam = models.CharField(
+        _("file name"),
+        max_length=255,
+        help_text=_("File name 'on disk' of the document, e.g. 'gelakt-verslag.pdf'."),
+        default="unknown.bin",  # TODO: remove this once we can enforce the blank=False
+    )
+    bestandsomvang = models.PositiveIntegerField(
+        _("file size"),
+        default=0,
+        help_text=_("Size of the file on disk, in bytes."),
+    )
+    registratiedatum = models.DateTimeField(
+        _("created on"),
+        auto_now_add=True,
+        editable=False,
+        help_text=_(
+            "System timestamp reflecting when the publication was registered in the "
+            "database. Not to be confused with the creation date of the publication, "
+            "which is usually *before* the registration date."
+        ),
+    )
+
+    class Meta:  # type: ignore
+        verbose_name = _("document")
+        verbose_name_plural = _("documents")
+
+    def __str__(self):
+        return self.officiele_titel
