30bd13e88ab8374d991bb7713240941eb25a7e2f
bart-maykin <bart.burgman@maykinmedia.nl>
2024-10-18 23:52:04 +0200

    :sparkles: [#26] added organisation api and added tests

--- src/woo_publications/metadata/api/serializer.py
+++ src/woo_publications/metadata/api/serializer.py
@@ -3,7 +3,8 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework_recursive.fields import RecursiveField
 
-from ..models import InformationCategory, Theme
+from ..constants import Origins
+from ..models import InformationCategory, Organisation, Theme
 
 
 class InformationCategorySerializer(serializers.ModelSerializer):
@@ -25,6 +26,39 @@ class InformationCategorySerializer(serializers.ModelSerializer):
                 )
             },
         }
+
+
+class OrganisationSerializer(serializers.ModelSerializer):
+    class Meta:  # type: ignore
+        model = Organisation
+        fields = ("uuid", "identifier", "naam", "oorsprong", "is_actief")
+        read_only_fields = (
+            "uuid",
+            "idenitfier",
+            "oorsprong",
+        )
+        extra_kwargs = {
+            "naam": {
+                "required": False,
+            },
+            "is_actief": {
+                "required": False,
+            },
+        }
+
+    def update(self, instance, validated_data):
+        if validated_data.get("naam") and instance.oorsprong != Origins.custom_entry:
+            raise serializers.ValidationError(
+                {
+                    "naam": _(
+                        "Only an organisation with the origin `{}` can update the `naam` field.".format(
+                            Origins.custom_entry
+                        )
+                    )
+                }
+            )
+
+        return super().update(instance, validated_data)
 
 
 class ThemeSerializer(serializers.ModelSerializer):
--- src/woo_publications/metadata/api/viewsets.py
+++ src/woo_publications/metadata/api/viewsets.py
@@ -1,11 +1,15 @@
 from django.utils.translation import gettext_lazy as _
 
 from drf_spectacular.utils import extend_schema, extend_schema_view
-from rest_framework import permissions, viewsets
+from rest_framework import mixins, permissions, viewsets
 
-from ..models import InformationCategory, Theme
-from .filters import InformationCategoryFilterSet
-from .serializer import InformationCategorySerializer, ThemeSerializer
+from ..models import InformationCategory, Organisation, Theme
+from .filters import InformationCategoryFilterSet, OrganisationFilterSet
+from .serializer import (
+    InformationCategorySerializer,
+    OrganisationSerializer,
+    ThemeSerializer,
+)
 
 
 @extend_schema(tags=["Informatiecategorieën"])
@@ -25,6 +29,43 @@ class InformationCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = InformationCategory.objects.all()
     serializer_class = InformationCategorySerializer
     filterset_class = InformationCategoryFilterSet
+    lookup_field = "uuid"
+    permission_classes = (permissions.AllowAny,)
+
+
+@extend_schema(tags=["Organisaties"])
+@extend_schema_view(
+    list=extend_schema(
+        summary=_("All available organisations."),
+        description=_("Returns a paginated result list of existing organisations."),
+    ),
+    retrieve=extend_schema(
+        summary=_("Retrieve a specific organisation."),
+        description=_("Retrieve a specific organisation."),
+    ),
+    create=extend_schema(
+        summary=_("Create a organisation."),
+        description=_("Create a organisation."),
+    ),
+    partial_update=extend_schema(
+        summary=_("Update a organisation partially."),
+        description=_("Update a organisation partially."),
+    ),
+    update=extend_schema(
+        summary=_("Update a organisation entirely."),
+        description=_("Update a organisation entirely."),
+    ),
+)
+class OrganisationViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Organisation.objects.all()
+    serializer_class = OrganisationSerializer
+    filterset_class = OrganisationFilterSet
     lookup_field = "uuid"
     permission_classes = (permissions.AllowAny,)
 
