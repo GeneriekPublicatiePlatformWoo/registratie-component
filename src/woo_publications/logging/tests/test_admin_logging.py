332ebff7b86141fc0843d6f09d649d9d2287331d
Sergei Maertens <sergei@maykinmedia.nl>
2024-10-21 15:28:19 +0200

    :art: [#16] Clean up admin logging tests

--- src/woo_publications/logging/tests/test_admin_logging.py
+++ src/woo_publications/logging/tests/test_admin_logging.py
@@ -22,10 +22,8 @@ class TestAdminAuditLogging(WebTest):
         super().setUpTestData()
         cls.user = UserFactory.create(superuser=True)
 
-    @freeze_time("2024-09-25T00:14:00-00:00")
     def test_admin_create(self):
         assert not TimelineLogProxy.objects.exists()
-
         reverse_url = reverse("admin:publications_publication_add")
 
         response = self.app.get(reverse_url, user=self.user)
@@ -40,7 +38,8 @@ class TestAdminAuditLogging(WebTest):
             "iaculis eu cursus sit amet, accumsan ac urna. Mauris interdum eleifend eros sed consectetur."
         )
 
-        form.submit(name="_save")
+        with freeze_time("2024-09-25T00:14:00-00:00"):
+            form.submit(name="_save")
 
         added_item = Publication.objects.order_by("-pk").first()
         assert added_item is not None
@@ -66,15 +65,14 @@ class TestAdminAuditLogging(WebTest):
 
         self.assertEqual(log.extra_data, expected_data)
 
-    @freeze_time("2024-09-27T00:14:00-00:00")
     def test_admin_update(self):
         assert not TimelineLogProxy.objects.exists()
-
-        publication = PublicationFactory.create(
-            officiele_titel="title one",
-            verkorte_titel="one",
-            omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        )
+        with freeze_time("2024-09-27T00:14:00-00:00"):
+            publication = PublicationFactory.create(
+                officiele_titel="title one",
+                verkorte_titel="one",
+                omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            )
         reverse_url = reverse(
             "admin:publications_publication_change",
             kwargs={"object_id": publication.id},
@@ -94,11 +92,9 @@ class TestAdminAuditLogging(WebTest):
         publication.refresh_from_db()
 
         self.assertEqual(TimelineLogProxy.objects.count(), 2)
+        read_log, update_log = TimelineLogProxy.objects.order_by("pk")
 
         with self.subTest("read audit logging"):
-            log = TimelineLogProxy.objects.first()
-            assert log is not None
-
             expected_data = {
                 "event": "read",
                 "acting_user": {
@@ -108,12 +104,9 @@ class TestAdminAuditLogging(WebTest):
                 "_cached_object_repr": "title one",
             }
 
-            self.assertEqual(log.extra_data, expected_data)
+            self.assertEqual(read_log.extra_data, expected_data)
 
         with self.subTest("update audit logging"):
-            log = TimelineLogProxy.objects.last()
-            assert log is not None
-
             expected_data = {
                 "event": "update",
                 "acting_user": {
@@ -131,16 +124,17 @@ class TestAdminAuditLogging(WebTest):
                 "_cached_object_repr": "changed official title",
             }
 
-            self.assertEqual(log.extra_data, expected_data)
+            self.assertEqual(update_log.extra_data, expected_data)
 
     def test_admin_delete(self):
         assert not TimelineLogProxy.objects.exists()
 
-        publication = PublicationFactory.create(
-            officiele_titel="title one",
-            verkorte_titel="one",
-            omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        )
+        with freeze_time("2024-09-27T00:14:00-00:00"):
+            publication = PublicationFactory.create(
+                officiele_titel="title one",
+                verkorte_titel="one",
+                omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            )
         reverse_url = reverse(
             "admin:publications_publication_delete",
             kwargs={"object_id": publication.id},
@@ -164,23 +158,29 @@ class TestAdminAuditLogging(WebTest):
                 "identifier": self.user.id,
                 "display_name": self.user.get_full_name(),
             },
+            "object_data": {
+                "id": publication.pk,
+                "uuid": str(publication.uuid),
+                "omschrijving": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "verkorte_titel": "one",
+                "officiele_titel": "title one",
+                "registratiedatum": "2024-09-27T00:14:00Z",
+            },
             "_cached_object_repr": "title one",
         }
 
         self.assertEqual(log.extra_data, expected_data)
 
-    @freeze_time("2024-09-25T00:14:00-00:00")
     def test_admin_inline_update_admin(self):
         assert not TimelineLogProxy.objects.exists()
 
-        publication = PublicationFactory.create(
-            officiele_titel="title one",
-            verkorte_titel="one",
-            omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        )
-        document = DocumentFactory.create(
-            publicatie=publication,
-        )
+        with freeze_time("2024-09-25T00:14:00-00:00"):
+            publication = PublicationFactory.create(
+                officiele_titel="title one",
+                verkorte_titel="one",
+                omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            )
+            document = DocumentFactory.create(publicatie=publication)
         reverse_url = reverse(
             "admin:publications_publication_change",
             kwargs={"object_id": publication.id},
@@ -191,8 +191,7 @@ class TestAdminAuditLogging(WebTest):
         self.assertEqual(response.status_code, 200)
 
         with self.subTest("read audit logging"):
-            log = TimelineLogProxy.objects.first()
-            assert log is not None
+            log = TimelineLogProxy.objects.get()
 
             expected_data = {
                 "event": "read",
@@ -241,17 +240,17 @@ class TestAdminAuditLogging(WebTest):
 
             self.assertEqual(log.extra_data, expected_data)
 
-    @freeze_time("2024-09-25T00:14:00-00:00")
     def test_admin_inline_create_admin(self):
         PublicationAdmin.inlines[0].extra = 1
         assert not TimelineLogProxy.objects.exists()
         assert not Document.objects.exists()
 
-        publication = PublicationFactory.create(
-            officiele_titel="title one",
-            verkorte_titel="one",
-            omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        )
+        with freeze_time("2024-09-25T00:14:00-00:00"):
+            publication = PublicationFactory.create(
+                officiele_titel="title one",
+                verkorte_titel="one",
+                omschrijving="Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            )
         reverse_url = reverse(
             "admin:publications_publication_change",
             kwargs={"object_id": publication.id},
@@ -262,8 +261,7 @@ class TestAdminAuditLogging(WebTest):
         self.assertEqual(response.status_code, 200)
 
         with self.subTest("read audit logging"):
-            log = TimelineLogProxy.objects.first()
-            assert log is not None
+            log = TimelineLogProxy.objects.get()
 
             expected_data = {
                 "event": "read",
@@ -280,15 +278,15 @@ class TestAdminAuditLogging(WebTest):
         form["document_set-0-identifier"] = "http://example.com/1"
         form["document_set-0-officiele_titel"] = "title"
         form["document_set-0-creatiedatum"] = "17-10-2024"
-        response = form.submit(name="_save")
+
+        with freeze_time("2024-09-26T00:14:00-00:00"):
+            response = form.submit(name="_save")
 
         self.assertEqual(response.status_code, 302)
 
         with self.subTest("update inline update logging"):
-            document = Document.objects.first()
-            assert document is not None
-            log = TimelineLogProxy.objects.filter(object_id=document.pk).first()
-            assert log is not None
+            document = Document.objects.get()
+            log = TimelineLogProxy.objects.filter(object_id=document.pk).get()
 
             expected_data = {
                 "event": "create",
@@ -307,7 +305,7 @@ class TestAdminAuditLogging(WebTest):
                     "verkorte_titel": "",
                     "bestandsformaat": "unknown",
                     "officiele_titel": "title",
-                    "registratiedatum": "2024-09-25T00:14:00Z",
+                    "registratiedatum": "2024-09-26T00:14:00Z",
                 },
                 "_cached_object_repr": "title",
             }
