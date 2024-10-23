85219e689ef9b3cdac1fbdc0f83fb7298af2cda0
bart-maykin <bart.burgman@maykinmedia.nl>
2024-10-23 07:15:22 +0200

    :sparkles: [#38] document api list endpoint

--- src/woo_publications/publications/api/filters.py
+++ src/woo_publications/publications/api/filters.py
@@ -2,7 +2,32 @@ from django.utils.translation import gettext_lazy as _
 
 from django_filters.rest_framework import FilterSet, filters
 
-from ..models import Publication
+from ..models import Document, Publication
+
+
+class DocumentFilterSet(FilterSet):
+    publication = filters.UUIDFilter(
+        field_name="publication__uuid",
+        lookup_expr="exact",
+        help_text=_(
+            "Search the document based on the unique identifier that represents a publication.."
+        ),
+    )
+    sorteer = filters.OrderingFilter(
+        help_text=_("Order on."),
+        fields=(
+            "creatiedatum",
+            "officiele_titel",
+            "verkorte_titel",
+        ),
+    )
+
+    class Meta:
+        model = Document
+        fields = (
+            "publication",
+            "sorteer",
+        )
 
 
 class PublicationFilterSet(FilterSet):
