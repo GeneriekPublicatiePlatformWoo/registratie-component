c73dba6717fab03c4438fd0e5ca31fb599b27258
bart-maykin <bart.burgman@maykinmedia.nl>
2024-10-23 16:41:07 +0200

    :art: [#26] improved text and tests

--- src/woo_publications/metadata/constants.py
+++ src/woo_publications/metadata/constants.py
@@ -7,12 +7,6 @@ class InformationCategoryOrigins(TextChoices):
     custom_entry = "zelf_toegevoegd", _("Custom entry")
 
 
-class OrganisationActive(TextChoices):
-    active = "true", _("Retrieves all the active organisations (default)")
-    inactive = "false", _("Retrieves all the inactive organisations")
-    all = "alle", _("Retrieve every organisation regardless if active or inactive.")
-
-
 class OrganisationOrigins(TextChoices):
     municipality_list = "gemeentelijst", _("Municipality list")
     so_list = "solijst", _("Collaborative organisations list")
