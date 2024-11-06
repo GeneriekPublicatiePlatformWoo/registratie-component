338c75d9fc9d6600a69c0ab9db7d47bccf9fa667
Sergei Maertens <sergei@maykinmedia.nl>
2024-11-06 15:19:02 +0100

    :adhesive_bandage: [#19] Avoid audit admin mixin crash by not using it
    There are two solutions to the problem:
    
    1. Fix or modify the mixin so that the crash is prevented. Looking up
       the object should then work differently
    2. Don't use the audit trail logging, but instead just rely on the
       admin logging
    
    Clearly I've picked approach 2. here, the reasoning is:
    
    * audit logging doesn't add much value over the admin log (we can see
      the new values, that's it)
    * the config is not exposed via an API endpoint, so we don't need a
      unified logging store (the admin log suffices)
    * we'd have to update the change_form template too, to make sure the
      'view logs' button is displayed (= more work for little value)
    * the configuration is not data that actually gets published to
      anywhere, the impact of changes to the outside world is fairly
      limited

--- src/woo_publications/config/admin.py
+++ src/woo_publications/config/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 
 from solo.admin import SingletonModelAdmin
 
-from woo_publications.logging.service import AdminAuditLogMixin
-
 from .models import GlobalConfiguration
 
 
-# FIXME: mixin order messes with singleton admin change_view override
 @admin.register(GlobalConfiguration)
-class GlobalConfigurationAdmin(AdminAuditLogMixin, SingletonModelAdmin):
+class GlobalConfigurationAdmin(SingletonModelAdmin):
     pass
