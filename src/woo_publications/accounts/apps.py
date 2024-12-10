a667fbd319618e5966607d24588b7a3ec3fcb133
bart-maykin <bart.burgman@maykinmedia.nl>
2024-12-10 11:19:21 +0100

    :sparkles: [#145] added generate_default_groups_fixture.sh file to dump the account groups, and added to the accounts/apps.py fun to load in the groups fixture file post migrate.

--- src/woo_publications/accounts/apps.py
+++ src/woo_publications/accounts/apps.py
@@ -1,6 +1,7 @@
 from io import StringIO
 
 from django.apps import AppConfig, apps
+from django.contrib.auth.management import create_permissions
 from django.contrib.contenttypes.management import create_contenttypes
 from django.core.management import call_command
 from django.db.models.signals import post_migrate
@@ -18,8 +19,17 @@ def update_admin_index(sender, **kwargs):
     call_command("loaddata", "default_admin_index", verbosity=0, stdout=StringIO())
 
 
+def update_groups(sender, **kwargs):
+    for app_config in apps.get_app_configs():
+        if app_config.name.startswith("woo_publications"):
+            create_permissions(app_config, verbosity=0)
+
+    call_command("loaddata", "default_groups", verbosity=0, stdout=StringIO())
+
+
 class AccountsConfig(AppConfig):
     name = "woo_publications.accounts"
 
     def ready(self):
         post_migrate.connect(update_admin_index, sender=self)
+        post_migrate.connect(update_groups, sender=self)
